947aac3039efc65edb2e6652cd644b2eae312f4d
MichaelPyrdol <michaelpyrdol@brandeis.edu>
2023-10-27 02:54:42 -0400

    Finished porting to express

--- Brandeis_SAA_Express/index.js
+++ Brandeis_SAA_Express/index.js
@@ -1,44 +1,18 @@
-const port = 8080;
-const http = require("http");
-const contentType=require("./contentType");
-const util=require("./util");
-router=require("./router")
-fs=require("fs"),
-router.get("/",(req,res)=>{
-    res.writeHead(200,htmlContentType);
-    customReadFile("views/index.html",res)
+const express = require("express");
+const homeController=require("./controller/homeController")
+const layouts=require("express-ejs-layouts")
+const app = express();
+app.set("view engine","ejs");
+app.use(layouts);
+app.use(express.static("public"));
+app.set("port",process.env.PORT||3000);
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.get("/",homeController.showIndex);
+app.get("/about",homeController.showAbout);
+app.get("/contact",homeController.showContact);
+app.get("/event",homeController.showEvent);
+app.get("/jobs",homeController.showJobs);
+app.listen(app.get("port"),()=>{
+    console.log(`Server running at http://localhost:${app.get("port")}`);
 });
-router.get("/index.html",(req,res)=>{
-    res.writeHead(200,htmlContentType);
-    customReadFile("views/index.html",res)
-});
-router.get("/about.html",(req,res)=>{
-    res.writeHead(200,htmlContentType);
-    customReadFile("views/about.html",res)
-});
-router.get("/jobs.html",(req,res)=>{
-    res.writeHead(200,htmlContentType);
-    customReadFile("views/jobs.html",res)
-});
-router.get("/event.html",(req,res)=>{
-    res.writeHead(200,htmlContentType);
-    customReadFile("views/event.html",res)
-});
-router.get("/contact.html",(req,res)=>{
-    res.writeHead(200,htmlContentType);
-    customReadFile("views/contact.html",res)
-});
-router.get("/public/css/style.css",(req,res)=>{
-    res.writeHead(200,cssContentType);
-    customReadFile("public/css/style.css",res)
-});
-router.get("/public/images/BrandeisLogo.png",(req,res)=>{
-    res.writeHead(200,cssContentType);
-    customReadFile("public/images/BrandeisLogo.png",res)
-});
-router.post("/",(req,res)=>{
-    res.writeHead(200,plainTextContentType);
-    res.end("POSTED");
-});
-http.createServer(router.handle).listen(8080);
-console.log(`The server is listening on port number:${port}`);
--- Brandeis_SAA_Express/util.js
+++
@@ -1,8 +0,0 @@
-customReadFile=(file,res)=>{
-    fs.readFile(`./${file}`,(errors,data)=>{
-        if(errors){
-            console.log("Error reading the file...");
-        }
-        res.end(data);
-    });
-};
